3373d1d6928f98692a0d3eb3de8a98276ed3d52c
Phonghotien <hotienphong120699@gmail.com>
2023-12-11 18:01:41 +0700

    phong-update-day

--- app/js/module/FileModule.js
+++ app/js/module/FileModule.js
@@ -8,4 +8,17 @@ export default function FileModule() {
   if (inputUpload) {
     inputUpload.addEventListener("change", loadFile);
   }
+  $("#imageUpload").change(function () {
+    readImgUrlAndPreview(this);
+    function readImgUrlAndPreview(input) {
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $("#imagePreview").attr("src", e.target.result);
+          $("#imagePreview").css("opacity", 1);
+        };
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  });
 }
